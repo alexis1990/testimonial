c82b602b5636c54dbd460e2a0d27455e3e35e495
Alexis Zrihen <a.zrihen@linkbynet.com>
2016-01-20 19:28:56 +0100

    Init Project

---
+++ testimonial/testimonial/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  testimonial
+//
+//  Created by Alexis ZRIHEN on 20/01/2016.
+//  Copyright © 2016 Alexis ZRIHEN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("test")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
